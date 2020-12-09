1c117180e79e71dd302bf0bccff79547559d2823
Alvaro Santillan <afsm1995@live.com>
2020-12-08 21:23:20 -0700

    Days Work Added Table View And Finalized Input Validation.

--- Password Manager/FirstViewController.swift
+++ Password Manager/FirstViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var list = ["a", "b"]
 
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var accountTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (list.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return (cell)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            list.remove(at: indexPath.row)
+            accountTableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        accountTableView.reloadData()
+    }
 
 }
 
--- Password Manager/SecondViewController.swift
+++ Password Manager/SecondViewController.swift
@@ -9,12 +9,67 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    @IBOutlet weak var accountNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var addAccountButton: GeneralUIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        addAccountButton.isEnabled = false
+//        print(addAccountButton.backgroundColor)
+        addAccountButton.backgroundColor = UIColor.gray
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func accountTextFieldPressed(_ sender: UITextField) {
+        let letters = NSCharacterSet.letters
+        let sd = accountNameTextField.text?.rangeOfCharacter(from: letters)
+        
+        if passwordTextField.text != nil && accountNameTextField.text != nil {
+            addAccountButton.isEnabled = true
+            addAccountButton.backgroundColor = UIColor(named: "Webroot")
+        } else {
+            addAccountButton.isEnabled = false
+            addAccountButton.backgroundColor = UIColor.gray
+        }
+    }
+    
+    @IBAction func passwordTextFieldPressed(_ sender: UITextField) {
+        if passwordTextField.text != nil && accountNameTextField.text != nil {
+            addAccountButton.isEnabled = true
+            addAccountButton.backgroundColor = UIColor(named: "Webroot")
+        } else {
+            addAccountButton.isEnabled = false
+            addAccountButton.backgroundColor = UIColor.gray
+//            addAccountButton.backgroundColor = UIColor(named: "Webroot")
+        }
+    }
+    
+    @IBAction func addAccountButtonPressed(_ sender: GeneralUIButton) {
+        var existingAccount = false
+        if accountNameTextField.text != nil {
+            for i in list {
+                if accountNameTextField.text == i {
+                    print(i, i.count)
+                    list.remove(at: i.count)
+                    list.insert(accountNameTextField.text ?? "O no bob", at: i.count)
+                    existingAccount = true
+                    passwordTextField.text = nil
+                    accountNameTextField.text = nil
+                    addAccountButton.isEnabled = false
+                    addAccountButton.backgroundColor = UIColor.gray
+                }
+            }
+            
+            if existingAccount == false {
+                list.append(accountNameTextField.text ?? "O no bob")
+                passwordTextField.text = nil
+                accountNameTextField.text = nil
+                addAccountButton.isEnabled = false
+                addAccountButton.backgroundColor = UIColor.gray
+            }
+        }
+    }
+    
 }
 
