185c0d1d8e1c76f9c08eb3ea60ab6822cd2dc3b9
Alvaro Santillan <afsm1995@live.com>
2020-12-10 17:17:04 -0700

    Face Id And Touch ID Now Work As Login Varification.

--- Password Manager/LoginScreenViewController.swift
+++ Password Manager/LoginScreenViewController.swift
@@ -7,20 +7,28 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class LoginScreenViewController: UIViewController {
     @IBOutlet weak var unlockButton: UIButton!
     @IBOutlet weak var passwordEntryTextField: UITextField!
     @IBOutlet weak var feedBackLabel: UILabel!
+    @IBOutlet weak var bioUnlockButton: GeneralUIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    
-    @IBAction func unlockButtonPressed(_ sender: Any) {
         print(KeychainWrapper.standard.string(forKey: "Password"))
         
+        let context = LAContext()
+        var error: NSError?
+        
+        if !(context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error)) {
+            // no biometrics on phone desable bio button
+            bioUnlockButton.isHidden = true
+        }
+    }
+    
+    @IBAction func unlockButtonPressed(_ sender: Any) {
         if KeychainWrapper.standard.string(forKey: "Password") == nil {
             self.performSegue(withIdentifier: "newPasswordSegue", sender: nil)
         } else if passwordEntryTextField.text != KeychainWrapper.standard.string(forKey: "Password") {
@@ -28,6 +36,36 @@ class LoginScreenViewController: UIViewController {
             passwordEntryTextField.text = nil
         } else {
             self.performSegue(withIdentifier: "unlockSegue", sender: nil)
+        }
+    }
+    
+    @IBAction func bioUnlockButtonPressed(_ sender: GeneralUIButton) {
+        if KeychainWrapper.standard.string(forKey: "Password") == nil {
+            self.performSegue(withIdentifier: "newPasswordSegue", sender: nil)
+        } else {
+            unlockUsingBiometrics()
+        }
+    }
+    
+    func unlockUsingBiometrics() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "To Unlock, Please varify its you"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] success, authenticationError in
+                DispatchQueue.main.async {
+                    if success {
+                        self?.performSegue(withIdentifier: "unlockSegue", sender: nil)
+                    }
+                }
+            }
+        } else {
+            // no biometrics on phone
+            let ac = UIAlertController(title: "Bio unavaliable", message: "not avalable on your phone", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
         }
     }
 }
